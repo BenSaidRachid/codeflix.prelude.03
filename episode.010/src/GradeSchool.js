fe7a3c8e262c112d73664fcd135ae1c63acfd127
Rachid BEN SAID <20190072@B15-01.local>
2019-11-08 07:57:16 +0100

    delete extra folders

--- episode.010/src/GradeSchool.js
+++ episode.010/src/GradeSchool.js
@@ -1,32 +1,13 @@
-export default class RobotName {
+export default class GradeSchool {
   constructor() {
-    this.arrRobot = [];
-    this.name = "";
-    this.regenerateName();
+    this.students = new Map();
   }
 
-  regenerateName() {
-      this.name = this.generateRandomAlpha(2) + this.generateRandomDigit(3);
-      if(this.arrRobot.indexOf(this.name) > 0){
-        this.regenerateName()
-      } else {
-        this.arrRobot.push(this.name)
-      }
+  addStudent(name, grade) {
+    this.students.set(grade, [name].sort());
   }
 
-  generateRandomAlpha(numberAlpha) {
-    let letters = "";
-    for(let i = 0; i < numberAlpha; i++) {
-      letters += String.fromCharCode(65+Math.floor(Math.random() * 26));
-    }
-    return letters;
-  }
-
-  generateRandomDigit(numberDigit) {
-    let digits = "";
-    for(let i = 0; i < numberDigit; i++) {
-      digits += Math.floor((Math.random() * 10));
-    }
-    return digits;
+  getAllStudents() {
+    return this.students;
   }
 }
